732bce28eafce0ed701b84955191b95eb4c92370
B4ckB0n3 <52764359+B4ckB0n3@users.noreply.github.com>
2023-03-29 06:51:23 -0400

    Create mod.ts

---
+++ mod.ts
@@ -0,0 +1,20 @@
+import { serve } from "https://deno.land/std@0.181.0/http/server.ts";
+import { Bot, webhookCallback } from "https://deno.land/x/grammy@v1.15.3/mod.ts";
+// Puedes modificar esto a la forma correcta de importar tu objeto `Bot`.
+import bot from "./bot.ts";
+
+const handleUpdate = webhookCallback(bot, "std/http");
+
+serve(async (req) => {
+  if (req.method === "POST") {
+    const url = new URL(req.url);
+    if (url.pathname.slice(1) === bot.token) {
+      try {
+        return await handleUpdate(req);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+  return new Response();
+});
