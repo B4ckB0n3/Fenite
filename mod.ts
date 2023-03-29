e50ca72afb83c4bcb2fac865963ed222b2d3b1e8
B4ckB0n3 <52764359+B4ckB0n3@users.noreply.github.com>
2023-03-29 07:22:03 -0400

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.181.0/http/server.ts";
 import { Bot, webhookCallback } from "https://deno.land/x/grammy@v1.15.3/mod.ts";
 // Puedes modificar esto a la forma correcta de importar tu objeto `Bot`.
-import bot from "./bot.ts";
+import bot from "./src/fenite.pl";
 
 const handleUpdate = webhookCallback(bot, "std/http");
 
